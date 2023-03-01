d8c5ff41f0fb838e425090c399cd716d72edf7c4
Hyungjin Kim <kimhyungjin0712@gmail.com>
2023-03-01 14:40:13 -0600

    add master base code

---
+++ src/app/imagesgeneration/imagesgeneration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagesgeneration',
+  templateUrl: './imagesgeneration.component.html',
+  styleUrls: ['./imagesgeneration.component.css']
+})
+export class ImagesgenerationComponent {
+
+}
---
+++ src/app/textdavinciedit001/textdavinciedit001.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textdavinciedit001',
+  templateUrl: './textdavinciedit001.component.html',
+  styleUrls: ['./textdavinciedit001.component.css']
+})
+export class Textdavinciedit001Component {
+
+}
